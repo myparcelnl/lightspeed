95f80f445ce10eb7250a9df5250b6f937f7f3c0b
Edie Lemoine <edie@myparcel.nl>
2020-12-14 16:56:16 +0100

    feat: language switching by url params

--- js/languagepicker.js
+++ js/languagepicker.js
@@ -1,5 +1,19 @@
+var languages = {
+    nl: '#NL',
+    en: '#EN',
+}
+
 $(document).ready(function () {
-    $('#NL').click();
+    var selector = '#NL';
+
+    var url = new URLSearchParams(window.location.search)
+    var lang = url.get('lang');
+
+    if (lang && Object.keys(languages).includes(lang)) {
+        selector = languages[lang]
+    }
+
+    $(selector).click();
 });
 
 /*
@@ -8,6 +22,13 @@ $(document).ready(function () {
 $(document).on("click", ".languagepicker > li", function () {
     var clickedLanguage = $(this).attr('id'); /* Get the id of the selected language */
     var lowerCase = clickedLanguage.toLowerCase();
+    var params = new URLSearchParams(window.location.search);
+
+    params.set('lang', lowerCase);
+
+    var location = new URL(window.location.href);
+    location.search = params.toString();
+    window.history.replaceState( {} , window.title, location);
 
     $(this).prependTo('ul.languagepicker'); /* set the selected language at first option */
     $(".all-content").load("language/" + lowerCase + "_" + clickedLanguage + ".html");
